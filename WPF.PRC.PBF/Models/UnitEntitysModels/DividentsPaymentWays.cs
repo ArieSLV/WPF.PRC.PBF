e74538f31f53ce0c9447b55a2c45e26d4955b7c4
Lev <arieslv@ya.ru>
2017-12-19 14:41:52 +0500

    Working on SuggestModule

---
+++ WPF.PRC.PBF/Models/UnitEntitysModels/DividentsPaymentWays.cs
@@ -0,0 +1,18 @@
+﻿namespace WPF.PRC.PBF
+{
+    /// <summary>
+    ///     Способ получения дивидентов
+    /// </summary>
+    public enum DividentsPaymentWays
+    {
+        /// <summary>
+        ///     Получение диведендов почтовым переводом
+        /// </summary>
+        ByMail,
+
+        /// <summary>
+        ///     Получение диведендов банковским переводом
+        /// </summary>
+        ByBank
+    }
+}
